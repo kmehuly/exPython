1021120fc9c1214796aed2087da791bc2ea7ee7d
Mehul Khandhadiya <53016215+mehulk-5245@users.noreply.github.com>
2020-10-09 00:05:25 +0530

    Installation of Packages and others

--- LiveScoreNotify.py
+++ LiveScoreNotify.py
@@ -1,7 +1,10 @@
+#pip3 install sports.py
+#pip3 install py-notifier
+
 import time
 import sports
 from pynotifier import Notification
 while(True):
     matchinfo = sports.get_sport("CRICKET")
     Notification(title = "IPL Live Score Update",description = str(matchinfo),duration = 10).send()
-    time.sleep(10)
+    time.sleep(1200)  #Notify after every 20 minutes
